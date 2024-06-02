619edca43b4e8eea93640c89f5de7be8c9ed1328
Teunvdk <teunvanderkleij@gmail.com>
2024-06-02 16:15:43 +0200

    removed styles

--- codeBlock.js
+++ codeBlock.js
@@ -5,14 +5,10 @@ class CodeBlock extends HTMLElement {
         mode: 'open'
       });
       this.imports = [
-       // 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/editor/editor.main.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/loader.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/editor/editor.main.nls.js',
         'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/editor/editor.main.js',
       ];
-      // this.cssImports = [
-      //   'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/editor/editor.main.min.css'
-      // ];
     }
   
     connectedCallback() {
@@ -242,17 +238,11 @@ class CodeBlock extends HTMLElement {
   
     
   loadMonacoResources() {
-    // Load CSS dynamically
-    // this.loadCSSResources(this.cssImports)
-    //   .then(() => {
-        // Load Monaco resources
         this.loadJSResources(this.imports)
           .then(() => {
             this.initializeMonacoEditor();
           })
           .catch(error => console.error('Failed to load Monaco Editor JS:', error));
-      // })
-      // .catch(error => console.error('Failed to load Monaco Editor CSS:', error));
   }
 
   loadCSSResources(urls) {
